808e1552dd2012e90421f8ab7c56169746a85c2e
BossMat <allouchemathieu-projectmanager@yahoo.fr>
2014-09-11 21:19:38 +0200

    J'ai ajouté une fonction

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("2eme ligne");		
+		System.out.println("2eme ligne");
+		System.out.println("3eme ligne")
 		
 
 	}
